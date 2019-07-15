952754c3855f0721447068a4847dd9c4fc7dd647
montini <energotronic1>
2019-07-15 10:48:24 +0300

    Updates

--- app/src/main/java/com/montini/teamsports/MainActivity.java
+++ app/src/main/java/com/montini/teamsports/MainActivity.java
@@ -1,21 +1,46 @@
 package com.montini.teamsports;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.sql.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ActionBar toolbar;
 
     static int numPlayers = 0;
     static int numFreeAgents = 0;
     static final boolean SINGLES = true;
     static final boolean DOUBLES = false;
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_event:
+                    return true;
+                case R.id.navigation_location:
+                    return true;
+                case R.id.navigation_player:
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navMain = findViewById(R.id.navigationView);
+        navMain.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         PlayEvent pe1 = new PlayEvent(
                 Date.valueOf("2019-07-13"),
