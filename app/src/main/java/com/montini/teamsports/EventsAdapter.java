bbf2fbdd7dfad20722213d27c38c2eb07d580739
montini <energotronic1>
2019-07-23 18:50:17 +0300

    RecyclerView is operational

--- app/src/main/java/com/montini/teamsports/EventsAdapter.java
+++ app/src/main/java/com/montini/teamsports/EventsAdapter.java
@@ -1,48 +1,93 @@
 package com.montini.teamsports;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventsAdapter extends ArrayAdapter<PlayEvent> {
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder> {
 
     private List<PlayEvent> data = new ArrayList<>();
     private Context mContext;
 
-    public EventsAdapter(Context context, ArrayList<PlayEvent> events) {
-        super(context, 0, events);
-        mContext = context;
-        data = events;
+    public EventsAdapter(List<PlayEvent> data, Context mContext) {
+        this.data = data;
+        this.mContext = mContext;
+        System.out.println(data.get(0).location.name);
+    }
+
+    // public EventsAdapter(Context context, ArrayList<PlayEvent> events) {
+    //     super(context, 0, events);
+    //     mContext = context;
+    //     data = events;
+    // }
+    //
+    // @Override
+    // public View getView(int position, View convertView, ViewGroup parent) {
+    //     View listItem = convertView;
+    //
+    //     // Check if an existing view is being reused, otherwise inflate the view
+    //     if (listItem == null) {
+    //         listItem = LayoutInflater.from(mContext).inflate(R.layout.events_list_row, parent, false);
+    //     }
+    //
+    //     // Get the data item for this position
+    //     // PlayEvent pe = getItem(position);
+    //     PlayEvent pe = data.get(position);
+    //
+    //     // Lookup view for data population
+    //     TextView tvDate = listItem.findViewById(R.id.eventDate);
+    //     TextView tvLocation = listItem.findViewById(R.id.eventLocation);
+    //
+    //     // Populate the data into the template view using the data object
+    //     tvDate.setText(pe.date.toString());
+    //     tvLocation.setText(pe.location.name);
+    //     System.out.println(tvDate.getText() + " / " + tvLocation.getText());
+    //
+    //     // Return the completed view to render on screen
+    //     return listItem;
+    // }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.events_list_row, viewGroup, false);
+        return new ViewHolder(v);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View listItem = convertView;
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        PlayEvent listItem = data.get(position);
 
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (listItem == null) {
-            listItem = LayoutInflater.from(mContext).inflate(R.layout.events_list_row, parent, false);
+        viewHolder.eDate.setText(listItem.date.toString());
+        viewHolder.eLocation.setText(listItem.location.name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView eDate;
+        public TextView eLocation;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            eDate = itemView.findViewById(R.id.eventDate);
+            eLocation = itemView.findViewById(R.id.eventLocation);
         }
-
-        // Get the data item for this position
-        // PlayEvent pe = getItem(position);
-        PlayEvent pe = data.get(position);
-
-        // Lookup view for data population
-        TextView tvDate = convertView.findViewById(R.id.eventDate);
-        TextView tvLocation = convertView.findViewById(R.id.eventLocation);
-
-        // Populate the data into the template view using the data object
-        tvDate.setText(pe.date.toString());
-        tvLocation.setText(pe.location.name);
-
-        // Return the completed view to render on screen
-        return listItem;
     }
 }
--- app/src/main/java/com/montini/teamsports/EventsFragment.java
+++ app/src/main/java/com/montini/teamsports/EventsFragment.java
@@ -1,14 +1,53 @@
 package com.montini.teamsports;
 
+import android.arch.lifecycle.OnLifecycleEvent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import java.sql.Date;
+import java.util.ArrayList;
 
 public class EventsFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_events, container, false);
+
+    private RecyclerView eventsListView;
+    private RecyclerView.Adapter adapter;
+    // ListView eventsListView;
+    // EventsAdapter adapter;
+
+    public EventsFragment() {
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_events, container, false);
+
+        eventsListView = (RecyclerView) v.findViewById(R.id.eventsList);
+        eventsListView.setHasFixedSize(true);
+        eventsListView.setLayoutManager(new LinearLayoutManager(v.getContext())); // v.getActivity()
+
+        ArrayList<PlayEvent> pe = new ArrayList<>();
+        pe.add(new PlayEvent(
+                Date.valueOf("2019-07-13"),
+                new Location("SEB arena", "Ąžuolyno g. 7, Vilnius", 2),
+                MainActivity.DOUBLES));
+
+        adapter = new EventsAdapter(pe, v.getContext());
+        // Log.e("WTF", v.getContext().toString());
+
+        eventsListView.setAdapter(adapter);
+
+        return v;
+    }
+
 }
